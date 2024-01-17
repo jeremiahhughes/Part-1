ba3165b8d537dd3f1ffb158e7135dc1216f9648a
jeremiahhughes <jeremiahhughes91@gmail.com>
2024-01-17 11:03:48 -0500

    Week 2, player moving, task 1, and task 2 completed

---
+++ Assets/Week 2/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector2 direction;
+    public Rigidbody2D rigidbody;
+    public float force = 10f;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.AddForce(direction * force * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Player hit by " +  collision.gameObject);
+    }
+}
